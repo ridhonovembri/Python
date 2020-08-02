3506686a4fa8de1598a9a43adc6b054d15f1ff56
ridhonovembri <ridhonovembri@gmail.com>
2020-08-02 11:00:30 +0700

    commit dari PC

---
+++ Main.py
@@ -0,0 +1,47 @@
+#INTEGER
+print("====INTEGER=====")
+data_int = 0
+print("data_int = ", data_int, "tipe datanya", type(data_int))
+
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
+print("data_float = ", data_float, "tipe datanya", type(data_float))
+print("data_string = ", data_str, "tipe datanya", type(data_str))
+print("data_boolean = ", data_bool,"tipe datanya", type(data_bool))
+
+#FLOAT
+print("====FLOAT=====")
+data_float = 0
+print("data_float = ", data_float, "tipe datanya", type(data_float))
+
+data_int = int(data_float)
+data_str = str(data_float)
+data_bool = bool(data_float)
+print("data_int = ", data_int, "tipe datanya", type(data_int))
+print("data_string = ", data_str, "tipe datanya", type(data_str))
+print("data_boolean = ", data_bool,  "tipe datanya", type(data_bool))
+
+#STRING
+print("====STRING=====")
+data_str = "10"
+print("data_string = ", data_str, "tipe datanya", type(data_str))
+
+data_int = int(data_str)
+data_float = float(data_str)
+data_bool = bool(data_str)
+print("data_int = ", data_int, "tipe datanya", type(data_int))
+print("data_float = ", data_float, "tipe datanya", type(data_float))
+print("data_boolean = ", data_bool, "tipe datanya", type(data_bool))
+
+#BOOLEAN
+print("====BOOL=====")
+data_bool = True
+print("data_bool = ", data_bool, "tipe datanya", type(data_bool))
+
+data_int = int(data_bool)
+data_float = float(data_bool)
+data_str = str(data_bool)
+print("data_int = ", data_int, "tipe datanya", type(data_int))
+print("data_float = ", data_float, "tipe datanya", type(data_float))
+print("data_str = ", data_str, "tipe datanya", type(data_str))
